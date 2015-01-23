817068169b36cdf62c251e085b2298666d53f598
unknown <KJ79607@PF00X3J0.Corp.Top.Wr.com>
2015-01-23 14:35:49 -0600

    Initial commit

---
+++ generateJSON.js
@@ -0,0 +1,33 @@
+/**
+ * Created by KJ79607 on 1/23/2015.
+ */
+
+var mysql      = require('mysql');
+var fs         = require('fs')
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'outagemap',
+    password : 'app',
+    database : 'mapping'
+});
+
+
+
+
+    connection.connect();
+
+    connection.query('SELECT * FROM outages', function (err, results) {
+        if (err) throw err;
+        //fs.writeFile("/curent.json", )
+        var output = ('{ "outages" : ' + JSON.stringify(results) + '}');
+        //console.log('outages: ', results);
+        fs.writeFile("\current.json", output, function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("outage file created at " + new Date().toString());
+            }
+        })
+    });
+
+    connection.end();
---
+++ start.js
@@ -0,0 +1,9 @@
+/**
+ * Created by KJ79607 on 1/23/2015.
+ */
+var generateJSON = require('./generateJSON.js');
+console.log("startjs process started at " + new Date().toString() );
+
+
+
+generateJSON.;
